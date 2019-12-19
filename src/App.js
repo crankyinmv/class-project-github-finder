d36c6e52d4a635e1035d007839b6690ff4eef68d
mike <crankyinmv@yahoo.com>
2019-12-18 20:22:18 -0800

    Lesson 32

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState,Fragment} from 'react';
-import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -12,27 +11,7 @@ import './App.css';
 
 const App = () =>
 {
-	const [repos, setRepos] = useState([]),
-	[loading, setLoading] = useState(false),
-	[alert, setAlert] = useState(null);
-
-	const gh_auth = `client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
-console.log(gh_auth);
-
-	const getUserRepos = async username => 
-	{
-//console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
-//console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
-//console.log('getUserRepos called');
-		setLoading(true);
-//		const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-		const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&${gh_auth}`);
-
-//console.log(`Also got: https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-		setRepos(res.data);
-		setLoading(false);
-	};
+	const [alert, setAlert] = useState(null);
 
 	const showAlert = (msg, type)=>
 	{
@@ -67,17 +46,7 @@ console.log(gh_auth);
 						)} 
 					/>
 					<Route exact path='/about' component={About} />
-					<Route 
-						exact 
-						path={`/user/:login`} 
-						render=					{						props=>						(
-							<User 
-								{...props} 
-								getUserRepos={getUserRepos} 
-								repos={repos} 
-							/>
-						)} 
-					/>
+					<Route exact path='/user/:login' component={User} /> 
 				</Switch>
 			</div>
 			<h2>Fuck Off</h2>
